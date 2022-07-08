8fc0d2d7be82330517d67e54b4b9990759ba1e3c
Nouman Hayat <muhammadnoumanhayat@gmail.com>
2022-07-08 13:30:27 +0500

    1st

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import {useFonts} from 'expo-font';
+import React from 'react';
+import {Text, View} from 'react-native';
+// import {DataProvider} from './src/hooks';
+
+// import AppNavigation from './src/navigation/App';
+const App = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default App;
