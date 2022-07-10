503dc26875ef93461107ddfea0ba40c687abbba7
Nouman Hayat <muhammadnoumanhayat@gmail.com>
2022-07-10 15:45:07 +0500

    Adding Drawer Navigation

--- App.tsx
+++ App.tsx
@@ -90,8 +90,6 @@ export default function App() {
   return (
     <DataProvider>
       <AppNavigation />
-    </DataProvider>
-   
-   
+    </DataProvider> 
   );
 }
--- src/navigation/Menu.js
+++ src/navigation/Menu.js
@@ -1,6 +1,7 @@
-// import React, {useCallback, useEffect, useRef, useState} from 'react';
-// import {Alert, Animated, Linking, StyleSheet} from 'react-native';
 
+// import React, { useCallback, useEffect, useRef, useState } from 'react';
+// import { Alert, Animated, Linking, StyleSheet } from 'react-native';
+// import * as t from '../constants/translations/en.json';
 // import {
 //   useIsDrawerOpen,
 //   createDrawerNavigator,
@@ -10,15 +11,16 @@
 // } from '@react-navigation/drawer';
 
 // import Screens from './Screens';
-// import {Block, Text, Switch, Button, Image} from '../components';
-// import {useData, useTheme, useTranslation} from '../hooks';
+// import { useData, useTheme } from '../hooks';
 
 // const Drawer = createDrawerNavigator();
+// import { FlatList, View } from 'react-native';
+// import { Block, Button, Image, Input, Product, Text } from '../components/';
+// import { Home } from '../screens';
 
-// /* drawer menu screens navigation */
 // const ScreensStack = () => {
-//   const {colors} = useTheme();
-//   const isDrawerOpen = useIsDrawerOpen(); 
+//   const { colors } = useTheme();
+//   const isDrawerOpen = useIsDrawerOpen();
 //   const animation = useRef(new Animated.Value(0)).current;
 
 //   const scale = animation.interpolate({
@@ -33,7 +35,7 @@
 
 //   const animatedStyle = {
 //     borderRadius: borderRadius,
-//     transform: [{scale: scale}],
+//     transform: [{ scale: scale }],
 //   };
 
 //   useEffect(() => {
@@ -60,16 +62,16 @@
 //     </Animated.View>
 //   );
 // };
-
-// /* custom drawer menu */
-// const DrawerContent = (
-//   props: DrawerContentComponentProps<DrawerContentOptions>,
+// //===================================================================================================
+// const DrawerContent = async (
+//   props
 // ) => {
-//   const {navigation} = props;
-//   const {t} = useTranslation();
-//   const {isDark, handleIsDark} = useData();
+//   const { translations, changeLanguage } = useData();
+
+//   const { navigation } = props;
+//   const { isDark, handleIsDark } = useData();
 //   const [active, setActive] = useState('Home');
-//   const {assets, colors, gradients, sizes} = useTheme();
+//   const { assets, colors, gradients, sizes } = useTheme();
 //   const labelColor = colors.text;
 
 //   const handleNavigation = useCallback(
@@ -84,20 +86,20 @@
 
 //   // screen list for Drawer menu
 //   const screens = [
-//     {name: t('screens.home'), to: 'Home', icon: assets.home},
-//     {name: t('screens.components'), to: 'Components', icon: assets.components},
-//     {name: t('screens.articles'), to: 'Articles', icon: assets.document},
-//     {name: t('screens.rental'), to: 'Pro', icon: assets.rental},
-//     {name: t('screens.profile'), to: 'Profile', icon: assets.profile},
-//     {name: t('screens.settings'), to: 'Pro', icon: assets.settings},
-//     {name: t('screens.register'), to: 'Register', icon: assets.register},
-//     {name: t('screens.extra'), to: 'Pro', icon: assets.extras},
-//     {name: t('screens.creater'), to: 'Creater', icon: assets.extras},
-//     {name: t('screens.buttonExample'), to: 'ButtonExample', icon: assets.extras},
-//     {name: t('screens.cardExample'), to: 'CardExample', icon: assets.extras},
-//     {name: t('screens.inputExample'), to: 'InputExample', icon: assets.extras},
-//     {name: t('screens.shopping'), to: 'Shopping', icon: assets.extras},
-//   ]; 
+//     { name: translations.screens.home, to: 'Home', icon: assets.home },
+//     { name: translations.screens.components, to: 'Components', icon: assets.components },
+//     { name: translations.screens.articles, to: 'Articles', icon: assets.document },
+//     { name: translations.screens.rental, to: 'Pro', icon: assets.rental },
+//     { name: translations.screens.profile, to: 'Profile', icon: assets.profile },
+//     { name: translations.screens.settings, to: 'Pro', icon: assets.settings },
+//     { name: translations.screens.register, to: 'Register', icon: assets.register },
+//     { name: translations.screens.extra, to: 'Pro', icon: assets.extras },
+//     { name: translations.screens.creater, to: 'Creater', icon: assets.extras },
+//     { name: translations.screens.buttonExample, to: 'ButtonExample', icon: assets.extras },
+//     { name: translations.screens.cardExample, to: 'CardExample', icon: assets.extras },
+//     { name: translations.screens.inputExample, to: 'InputExample', icon: assets.extras },
+//     { name: translations.screens.shopping, to: 'Shopping', icon: assets.extras },
+//   ];
 
 //   return (
 //     <DrawerContentScrollView
@@ -105,7 +107,7 @@
 //       scrollEnabled
 //       removeClippedSubviews
 //       renderToHardwareTextureAndroid
-//       contentContainerStyle={{paddingBottom: sizes.padding}}>
+//       contentContainerStyle={{ paddingBottom: sizes.padding }}>
 //       <Block paddingHorizontal={sizes.padding} >
 //         <Block flex={0} row align="center" marginBottom={sizes.l}>
 //           <Image
@@ -118,10 +120,10 @@
 //           />
 //           <Block>
 //             <Text size={12} color={colors.contrasting} semibold>
-//               {t('app.name')}
+//               {translations.app.name}
 //             </Text>
 //             <Text size={12} color={colors.contrasting} semibold>
-//               {t('app.native')}
+//               {translations.app.native}
 //             </Text>
 //           </Block>
 //         </Block>
@@ -168,7 +170,7 @@
 //         />
 
 //         <Text semibold transform="uppercase" opacity={0.5}>
-//           {t('menu.documentation')}
+//           {translations.menu.documentation}
 //         </Text>
 
 //         <Button
@@ -177,7 +179,7 @@
 //           marginTop={sizes.sm}
 //           marginBottom={sizes.s}
 //           onPress={() =>
-//             handleWebLink('https://github.com/creativetimofficial')
+//             handleWebLink('https://github.com/NoumanHayat')
 //           }>
 //           <Block
 //             flex={0}
@@ -197,12 +199,12 @@
 //             />
 //           </Block>
 //           <Text p color={colors.contrasting}>
-//             {t('menu.started')}
+//             {translations.menu.started}
 //           </Text>
 //         </Button>
 
 //         <Block row justify="space-between" marginTop={sizes.sm}>
-//           <Text color={colors.contrasting}>{t('darkMode')}</Text>
+//           <Text color={colors.contrasting}>{translations.darkMode}</Text>
 //           <Switch
 //             checked={isDark}
 //             onPress={(checked) => {
@@ -216,16 +218,43 @@
 //   );
 // };
 
-// /* drawer menu navigation */
+
+//===================================================================================================
+// const App = () => {
+//   const { translations, changeLanguage } = useData();
+//   console.log("okk=" + translations.creater.name);
+
+//   const { assets, colors, fonts, gradients, sizes } = useTheme();
+//   return (
+//     <Block safe>
+//       <Text h3 style={{ fontFamily: 'OpenSans-Regular' }}>{translations.creater.name}</Text>
+//       <Button
+//         gradient={gradients.primary}
+//         marginBottom={sizes.base}
+//         onPress={() => {
+//           changeLanguage()
+//         }}>
+//         <Text white bold transform="uppercase">
+//           Change Language
+//         </Text>
+//       </Button>
+//     </Block>
+//   );
+// };
+
+// export default App;
+
+
+
 // export default () => {
-//   const {gradients} = useTheme();
+//   const { gradients } = useTheme();
 
 //   return (
 //     <Block gradient={gradients.drawer}>
 //       <Drawer.Navigator
 //         drawerType="slide"
 //         overlayColor="transparent"
-//         sceneContainerStyle={{backgroundColor: 'transparent'}}
+//         sceneContainerStyle={{ backgroundColor: 'transparent' }}
 //         drawerContent={(props) => <DrawerContent {...props} />}
 //         drawerStyle={{
 //           flex: 1,
@@ -238,275 +267,57 @@
 //     </Block>
 //   );
 // };
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Alert, Animated, Linking, StyleSheet } from 'react-native';
-import * as t from '../constants/translations/en.json';
-import {
-  useIsDrawerOpen,
-  createDrawerNavigator,
-  DrawerContentComponentProps,
-  DrawerContentOptions,
-  DrawerContentScrollView,
-} from '@react-navigation/drawer';
-
-import Screens from './Screens';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import { useData, useTheme } from '../hooks';
+import { Home, Components } from '../screens';
+function HomeScreen({ navigation }) {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
 
 const Drawer = createDrawerNavigator();
-import { FlatList, View } from 'react-native';
-import { Block, Button, Image, Input, Product, Text } from '../components/';
-import { Home } from '../screens';
-const ScreensStack = () => {
-  const { colors } = useTheme();
-  const isDrawerOpen = useIsDrawerOpen();
-  const animation = useRef(new Animated.Value(0)).current;
 
-  const scale = animation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [1, 0.88],
-  });
-
-  const borderRadius = animation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, 16],
-  });
-
-  const animatedStyle = {
-    borderRadius: borderRadius,
-    transform: [{ scale: scale }],
-  };
-
-  useEffect(() => {
-    Animated.timing(animation, {
-      duration: 200,
-      useNativeDriver: true,
-      toValue: isDrawerOpen ? 1 : 0,
-    }).start();
-  }, [isDrawerOpen, animation]);
-
-  return (
-    <Animated.View
-      style={StyleSheet.flatten([
-        animatedStyle,
-        {
-          flex: 1,
-          overflow: 'hidden',
-          borderColor: colors.card,
-          borderWidth: isDrawerOpen ? 1 : 0,
-        },
-      ])}>
-      {/*  */}
-      <Screens />
-    </Animated.View>
-  );
-};
-//===================================================================================================
-const DrawerContent = async (
-  props
-) => {
-
-  const { navigation } = props;
-  const { isDark, handleIsDark } = useData();
-  const [active, setActive] = useState('Home');
-  const { assets, colors, gradients, sizes } = useTheme();
-  const labelColor = colors.text;
-
-  const handleNavigation = useCallback(
-    (to) => {
-      setActive(to);
-      navigation.navigate(to);
-    },
-    [navigation, setActive],
-  );
-
-  const handleWebLink = useCallback((url) => Linking.openURL(url), []);
-
-  // screen list for Drawer menu
-  const screens = [
-    { name: t('screens.home'), to: 'Home', icon: assets.home },
-    { name: t('screens.components'), to: 'Components', icon: assets.components },
-    { name: t('screens.articles'), to: 'Articles', icon: assets.document },
-    { name: t('screens.rental'), to: 'Pro', icon: assets.rental },
-    { name: t('screens.profile'), to: 'Profile', icon: assets.profile },
-    { name: t('screens.settings'), to: 'Pro', icon: assets.settings },
-    { name: t('screens.register'), to: 'Register', icon: assets.register },
-    { name: t('screens.extra'), to: 'Pro', icon: assets.extras },
-    { name: t('screens.creater'), to: 'Creater', icon: assets.extras },
-    { name: t('screens.buttonExample'), to: 'ButtonExample', icon: assets.extras },
-    { name: t('screens.cardExample'), to: 'CardExample', icon: assets.extras },
-    { name: t('screens.inputExample'), to: 'InputExample', icon: assets.extras },
-    { name: t('screens.shopping'), to: 'Shopping', icon: assets.extras },
-  ];
-
-  return (
-    <DrawerContentScrollView
-      {...props}
-      scrollEnabled
-      removeClippedSubviews
-      renderToHardwareTextureAndroid
-      contentContainerStyle={{ paddingBottom: sizes.padding }}>
-      <Block paddingHorizontal={sizes.padding} >
-        <Block flex={0} row align="center" marginBottom={sizes.l}>
-          <Image
-            radius={0}
-            width={33}
-            height={33}
-            color={colors.contrasting}
-            source={assets.logo}
-            marginRight={sizes.sm}
-          />
-          <Block>
-            <Text size={12} color={colors.contrasting} semibold>
-              {t('app.name')}
-            </Text>
-            <Text size={12} color={colors.contrasting} semibold>
-              {t('app.native')}
-            </Text>
-          </Block>
-        </Block>
-
-        {screens?.map((screen, index) => {
-          const isActive = active === screen.to;
-          return (
-            <Button
-              row
-              justify="flex-start"
-              marginBottom={sizes.s}
-              key={`menu-screen-${screen.name}-${index}`}
-              onPress={() => handleNavigation(screen.to)}>
-              <Block
-                flex={0}
-                radius={6}
-                align="center"
-                justify="center"
-                width={sizes.md}
-                height={sizes.md}
-                marginRight={sizes.s}
-                gradient={gradients[isActive ? 'primary' : 'white']}>
-                <Image
-                  radius={0}
-                  width={14}
-                  height={14}
-                  source={screen.icon}
-                  color={colors[isActive ? 'white' : 'black']}
-                />
-              </Block>
-              <Text p semibold={isActive} color={colors.contrasting}>
-                {screen.name}
-              </Text>
-            </Button>
-          );
-        })}
-
-        <Block
-          flex={0}
-          height={1}
-          marginRight={sizes.md}
-          marginVertical={sizes.sm}
-          gradient={gradients.menu}
-        />
-
-        <Text semibold transform="uppercase" opacity={0.5}>
-          {t('menu.documentation')}
-        </Text>
-
-        <Button
-          row
-          justify="flex-start"
-          marginTop={sizes.sm}
-          marginBottom={sizes.s}
-          onPress={() =>
-            handleWebLink('https://github.com/NoumanHayat')
-          }>
-          <Block
-            flex={0}
-            radius={6}
-            align="center"
-            justify="center"
-            width={sizes.md}
-            height={sizes.md}
-            marginRight={sizes.s}
-            gradient={gradients.white}>
-            <Image
-              radius={0}
-              width={14}
-              height={14}
-              color={colors.contrasting}
-              source={assets.documentation}
-            />
-          </Block>
-          <Text p color={colors.contrasting}>
-            {t('menu.started')}
-          </Text>
-        </Button>
-
-        <Block row justify="space-between" marginTop={sizes.sm}>
-          <Text color={colors.contrasting}>{t('darkMode')}</Text>
-          <Switch
-            checked={isDark}
-            onPress={(checked) => {
-              handleIsDark(checked);
-
-            }}
-          />
-        </Block>
-      </Block>
-    </DrawerContentScrollView>
-  );
-};
-
-
-//===================================================================================================
-const App = () => {
+export default function App() {
   const { translations, changeLanguage } = useData();
-  console.log("okk=" + translations.creater.name);
-
-  const { assets, colors, fonts, gradients, sizes } = useTheme();
   return (
-    <Block safe>
-      <Text h3 style={{ fontFamily: 'OpenSans-Regular' }}>{translations.creater.name}</Text>
-      <Button
-        gradient={gradients.primary}
-        marginBottom={sizes.base}
-        onPress={() => {
-          changeLanguage()
-        }}>
-        <Text white bold transform="uppercase">
-          Change Language
-        </Text>
-      </Button>
-    </Block>
+    // <NavigationContainer>
+    //   <Drawer.Navigator initialRouteName="Home">
+    //     <Drawer.Screen name={translations.screens.home} component={Home} />
+    //     <Drawer.Screen name={translations.screens.components} component={Components} />
+    //   </Drawer.Navigator>
+    // // </NavigationContainer>
+    <Drawer.Navigator
+    // drawerType="slide"
+    // overlayColor="transparent"
+    // sceneContainerStyle={{ backgroundColor: 'transparent' }}
+    // drawerContent={(props) => <DrawerContent {...props} />}
+    // drawerStyle={{
+    //   flex: 1,
+    //   width: '60%',
+    //   borderRightWidth: 0,
+    //   backgroundColor: 'transparent',
+    // }}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Components" component={Components} />
+    </Drawer.Navigator>
   );
-};
-
-export default App;
-
-
-// export default () => {
-//   const {gradients} = useTheme();
-
-//   return (
-//     <Block gradient={gradients.drawer}>
-//       <Drawer.Navigator
-//         drawerType="slide"
-//         overlayColor="transparent"
-//         sceneContainerStyle={{backgroundColor: 'transparent'}}
-//         drawerContent={(props) => <DrawerContent {...props} />}
-//         drawerStyle={{
-//           flex: 1,
-//           width: '60%',
-//           borderRightWidth: 0,
-//           backgroundColor: 'transparent',
-//         }}>
-//         <Drawer.Screen name="Screens" component={ScreensStack} />
-//       </Drawer.Navigator>
-//     </Block>
-//   );
-// };
+}
